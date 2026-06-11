23e53a00accd50f4f2b90d16791e3eb223774514
User <you@example.com>
2026-06-11 09:40:58 +0200

    chore: bump version to 1.0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.havenapp.main"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.0.16"
+        versionCode = 20
+        versionName = "1.0.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
